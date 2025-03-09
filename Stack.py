dfef1ccbb4837ca2f96099e9e447d62cc8d319b4
Selalelo <117808094+Selalelo@users.noreply.github.com>
2025-03-09 12:12:27 +0000

    Third commit

--- Stack.py
+++ Stack.py
@@ -1,21 +1,23 @@
 class Stack:
     def __init__(self):
         self.stack = list()
-    
+        
+    def push(self, data):
+        self.stack.append(data)
+
     def pop(self):
         if len(self.stack) > 0:
              self.stack.pop()
         else:
             return None
         
-    def push(self, data):
-        self.stack.append(data)
-
+   
     def peek(self):
         if len(self.stack) > 0:
             return self.stack[len(self.stack) - 1]
         else:
             return None
+        
     def __str__(self):
         return str(self.stack)
     
